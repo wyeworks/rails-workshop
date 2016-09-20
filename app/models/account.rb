4bc6f75019a7a225a8fe2bb4e736c1d11613a38a
Jorge Bejar <jorge@wyeworks.com>
2016-09-20 16:36:05 -0300

    Agregamos validaciones al modelo Account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,6 @@
 class Account < ApplicationRecord
+  validates :name, presence: true
+  validates :kind, inclusion: { in: %w(bank cash credit-card) }
+  validates :balance, numericality: true
+  validates :currency, inclusion: { in: %w(USD UYU ARS) }
 end
