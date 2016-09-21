760cde67352a359adb2b4040fc17237c73fa7ec1
Jorge Bejar <jorge@wyeworks.com>
2016-09-21 10:56:19 -0300

    Agregamos un test de integración para la creación de Accounts

---
+++ test/integration/create_account_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class CreateAccountTest < ActionDispatch::IntegrationTest
+  test 'can create an account' do
+    get '/accounts/new'
+    assert_response :success
+
+    post '/accounts', params: {
+      account: {
+        name: 'Bank account',
+        kind: 'bank',
+        balance: 300,
+        currency: 'USD'
+      }
+    }
+
+    assert_response :redirect
+    follow_redirect!
+
+    assert_response :success
+    assert_select 'tbody tr:last-child' do
+      assert_select 'td:nth-child(1)', 'Bank account'
+      assert_select 'td:nth-child(2)', 'bank'
+      assert_select 'td:nth-child(3)', 'USD 300.00'
+    end
+  end
+end
