79a5601e5f6bf3f85bdd7cf317c63cb5eb3992f5
Jorge Bejar <jorge@wyeworks.com>
2016-09-22 11:30:52 -0300

    Controller y rutas para las entries

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :accounts, except: :show
+
+  resources :accounts, except: :show do
+    resources :entries, except: :show
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'accounts#index'
