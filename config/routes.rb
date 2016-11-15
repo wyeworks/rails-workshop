8973fdad859350bfe8f0947b0dd92c33166d164b
Santiago Pastorino <santiago@wyeworks.com>
2016-11-15 19:59:51 -0300

    Add entries controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :accounts, except: :show
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :accounts, except: :show do
+    resources :entries
+  end
 
   root 'accounts#index'
 end
