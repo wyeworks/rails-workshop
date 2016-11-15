5e1d35a3a84d73f971ec7fba98b8882e6b10a197
Santiago Pastorino <santiago@wyeworks.com>
2016-11-15 17:22:33 -0300

    Use select inputs for kind and currency

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def account_kind_options
+    [
+      ['Bank', 'bank'],
+      ['Cash', 'cash'],
+      ['Credit card', 'credit-card'],
+    ]
+  end
+
+  def currency_options
+    [
+      ['Dollars', 'USD'],
+      ['Uruguayan Pesos', 'UYU'],
+      ['Argentine Pesos', 'ARS']
+    ]
+  end
 end
