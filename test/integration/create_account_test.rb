06db90b629b016f9f8224a5b457c695e27105091
Santiago Pastorino <santiago@wyeworks.com>
2016-11-15 19:18:42 -0300

    Add account creation integration test

---
+++ test/integration/create_account_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class CreateAccountTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  test 'can create an account' do
+    sign_in users(:one)
+
+    get '/accounts/new'
+    assert_response :success
+
+    post '/accounts', params: {
+      account: {
+        name: 'Bank account',
+        kind: 'bank',
+        balance: 300,
+        currency: 'USD'
+      }
+    }
+
+    assert_response :redirect
+    follow_redirect!
+
+    assert_response :success
+    assert_select 'tbody tr:last-child' do
+      assert_select 'td:nth-child(1)', 'Bank account'
+      assert_select 'td:nth-child(2)', 'bank'
+      assert_select 'td:nth-child(3)', '300.0'
+    end
+  end
+end
